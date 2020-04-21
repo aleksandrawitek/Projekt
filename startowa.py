b5088bd2596460ee8a8424b85d2a84b86ecd68b5
aleksandrawitek <olciawitek@wp.pl>
2020-04-21 13:21:47 +0200

    Raport 2

--- startowa.py
+++ startowa.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk
-
+import os
 
 
 class Window(Frame):
@@ -14,12 +14,13 @@ class Window(Frame):
 
         #nadanie tytułu      
         self.master.title("Platforma Edukacyjna")
-
+    
 
 
 root = Tk()
 
 #rozmiar okna, dostosowany do wymiarów ekranu uzytkownika
+
 ekran_dl = root.winfo_screenwidth()
 ekran_szer = root.winfo_screenheight()
 okno_dl = 0.9*ekran_dl
@@ -32,11 +33,34 @@ app = Window(root)
 
 #dopracowanie detali wyglądu
 
-tekst = Label(root, text = 'Wybierz etap edukacji:', font = "Arial 30 ", width = okno_szer, height=4,background='MistyRose3', fg = 'misty rose', anchor = CENTER)
+tekst = Label(root, text = 'Wybierz etap edukacji:', font = "Arial 30 ", width = okno_szer, height=4,background='CadetBlue3', fg = 'PaleTurquoise1', anchor = CENTER)
 tekst.pack()
 
-root.configure(background='misty rose')
+root.configure(background='PaleTurquoise1')
 
-podstawowka = Button(root, text = 'Szkoła Podstawowa')
-podstawowka.pack()
+#dodanie przycisków oraz dopracowanie szczegółów ich wyglądu w zaleznosci od wielkosci okna
+
+okno_dl = int(okno_dl)
+okno_szer= int(okno_szer)
+
+buttonwidth = int(0.05*okno_dl)
+buttonheight = int(0.005*okno_szer)
+button1x = 0.35*okno_szer
+button1y = 0.15*okno_dl
+button2x = 0.35*okno_szer
+button2y = 0.25*okno_dl
+
+podstawowka = Button(root, text = 'Szkoła Podstawowa', width=buttonwidth, height=buttonheight, font = "Arial 20").place(x= button1x, y=button1y)
+liceum = Button(root, text = 'Liceum', width=buttonwidth, height=buttonheight, font = "Arial 20").place(x= button2x, y=button2y)
+
+#dodanie zdjęcia u dołu strony dopasowanego do wymiarów okna aplikacji
+base_folder = os.path.dirname(__file__)
+image_path = os.path.join(base_folder, 'brain.png')
+brain = PhotoImage(file=image_path)
+
+label = Label(image=brain, width = okno_dl, height = 0.3 * okno_szer)
+label.image = brain
+label.pack(side = BOTTOM)
+
+
 root.mainloop()  
